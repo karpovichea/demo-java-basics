3282344ffa6f41469485aa79f32d4378e2dd00e1
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-19 12:35:03 +0300

    add method getStringValueOfNumber

--- src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
+++ src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
@@ -2,4 +2,24 @@ package by.itacademy.ekaterinakarpovich.numbers;
 
 public class Main {
 
+    public static void main(String[] args) {
+        int number = 9;
+        String numberAsAString = "";
+    }
+
+    public static String getStringValueOfNumber(int number) {
+        switch (number) {
+            case(0): return "ноль";
+            case(1): return "один";
+            case(2): return "два";
+            case(3): return "три";
+            case(4): return "четыре";
+            case(5): return "пять";
+            case(6): return "шесть";
+            case(7): return "семь";
+            case(8): return "восемь";
+            case(9): return "девять";
+            default: return "Неверное число";
+        }
+    }
 }

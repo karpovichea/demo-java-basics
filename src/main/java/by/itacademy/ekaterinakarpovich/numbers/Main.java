b25fc59bba691eb4cdce6c468b344833cd256cac
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-20 21:06:25 +0300

    refactoring class Main

--- src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
+++ src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
@@ -3,10 +3,6 @@ package by.itacademy.ekaterinakarpovich.numbers;
 public class Main {
 
     public static void main(String[] args) {
-        int number = 9;
-        String numberAsAString = NumberUtils.getStringValue(number);
-        System.out.println(numberAsAString);
-
         for (int i = 0; i < 10; i++) {
             System.out.println(NumberUtils.getStringValue(i));
         }

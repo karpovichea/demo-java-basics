88cf162e3566935ee08726893065fe109065b414
Ekaterina Karpovich <kattykarpovich55@gmail.com>
2024-10-20 21:03:29 +0300

    refactoring class Main

--- src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
+++ src/main/java/by/itacademy/ekaterinakarpovich/numbers/Main.java
@@ -4,25 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         int number = 9;
-        String numberAsAString;
-
-        numberAsAString = getStringValueOfNumber(number);
+        String numberAsAString = NumberUtils.getStringValue(number);
         System.out.println(numberAsAString);
-    }
 
-    public static String getStringValueOfNumber(int number) {
-        switch (number) {
-            case(0): return "ноль";
-            case(1): return "один";
-            case(2): return "два";
-            case(3): return "три";
-            case(4): return "четыре";
-            case(5): return "пять";
-            case(6): return "шесть";
-            case(7): return "семь";
-            case(8): return "восемь";
-            case(9): return "девять";
-            default: return "Неверное число";
+        for (int i = 0; i < 10; i++) {
+            System.out.println(NumberUtils.getStringValue(i));
         }
     }
 }
